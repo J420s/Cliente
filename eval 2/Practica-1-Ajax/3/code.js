948875cc1788a208b1f89bf351f15891109cc4f5
J420s <aluseg230@ieselcaminas.org>
2020-12-01 22:38:59 +0100

    ajax-1-3

---
+++ eval 2/Practica-1-Ajax/3/code.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    xhr = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP")
+    button = document.getElementById('btn-btn')
+    addEvent(button,"click",()=>{swapIMG('img/ladyonstage.jpg','image')})
+}
+
+function swapIMG(new_img, img_elementID) {
+    if (xhr) {
+        xhr.open("GET", new_img);
+        xhr.responseType = 'blob';
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                let element = document.getElementById(img_elementID)
+                let url = window.URL || window.webkitURL
+                element.src = url.createObjectURL(xhr.response)
+            }
+        }
+        xhr.send(null);
+    }
+}
+
+
+function addEvent(element,event,foo){
+    addEventListener ? element.addEventListener(event,foo) : element.attachEvent("on"+event,foo)
+}
+
+
+
